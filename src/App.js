dbb293a11215bbfbeca0cf3dbaac3665ec859b53
Abhiraj3112000 <chatterjeeabhirajofficial@gmail.com>
2021-08-06 12:23:44 +0530

    added refresh button

--- src/App.js
+++ src/App.js
@@ -6,21 +6,37 @@ const baseUrl = `https://restcountries.eu/rest/v2/region/asia`;
 
 class App extends React.Component {
   state = { states: [], mounted: false };
+
   componentWillMount() {
+    this.apicall();
+  }
+
+  //Fetching data form REST-API
+  apicall() {
     axios.get(baseUrl).then((res) => {
       this.setState({ states: res.data });
       this.setState({ mounted: true });
-      console.log(this.state.states, this.state.mounted);
     });
   }
 
-  //capital, flag(display image for each country), region,
-  //subregion, population, borders & languages.
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          {/* <h1>{this.state.mounted?this.state.states[0].name:""}</h1> */}
+          <button
+            style={{
+              marginTop: "30px",
+              width: "200px",
+              height: "50px",
+              borderRadius: "5px",
+            }}
+            onClick={() => {
+              this.apicall();
+              alert("Data refreshed from API");
+            }}
+          >
+            <b>Refresh API</b>
+          </button>
           {this.state.mounted
             ? this.state.states.map((eachState) => {
                 return (
@@ -36,29 +52,62 @@ class App extends React.Component {
                     <img
                       src={eachState.flag}
                       style={{ height: "200px", width: "300px" }}
-                      alt="flag"
+                      alt="Flag"
                     />
-                    <p key={eachState.name}>Name: {eachState.name}</p>
-                    <p key={eachState.name}>Capital: {eachState.capital}</p>
-                    <p key={eachState.name}>Region: {eachState.region}</p>
-                    <p key={eachState.name}>Sub-region: {eachState.subregion}</p>
-                    <p key={eachState.name}>Population: {eachState.population}</p>
-                    <p key={eachState.name}>Borders:<br/>
-                      {eachState.borders.map((eachBorder, index) => {
-                        return <span key={index}>{eachBorder}&nbsp;&nbsp;</span>;
-                      })}
+                    <p key={eachState.name}>
+                      <b>Name:</b> {eachState.name}
                     </p>
                     <p key={eachState.name}>
-                      Languages:<br/>
-                      {eachState.languages.map((eachLanguage, index) => {
-                        return<>
-                            <p key={index}>iso639_1: {eachLanguage.iso639_1}</p>
-                            <p key={index}>iso639_2: {eachLanguage.iso639_2}</p>
-                            <p key={index}>name: {eachLanguage.name}</p>
-                            <p key={index}>nativeName: {eachLanguage.nativeName}</p>
-                        </>;
+                      <b>Capital: </b>
+                      {eachState.capital}
+                    </p>
+                    <p key={eachState.name}>
+                      <b>Region: </b>
+                      {eachState.region}
+                    </p>
+                    <p key={eachState.name}>
+                      <b>Sub-region: </b>
+                      {eachState.subregion}
+                    </p>
+                    <p key={eachState.name}>
+                      <b>Population: </b>
+                      {eachState.population}
+                    </p>
+                    <p key={eachState.name}>
+                      <b>Borders:</b>
+                      <br />
+                      <br />
+                      {eachState.borders.map((eachBorder, index) => {
+                        return (
+                          <span key={index}>{eachBorder}&nbsp;&nbsp;</span>
+                        );
                       })}
                     </p>
+
+                    <p key={eachState.name}>
+                      <b>Languages:</b>
+                      <br />
+                      <hr style={{ width: "50%" }} />
+                      {eachState.languages.map((eachLanguage, index) => {
+                        return (
+                          <>
+                            <p key={eachLanguage.index}>
+                              iso639_1: {eachLanguage.iso639_1}
+                            </p>
+                            <p key={eachLanguage.index}>
+                              iso639_2: {eachLanguage.iso639_2}
+                            </p>
+                            <p key={eachLanguage.index}>
+                              name: {eachLanguage.name}
+                            </p>
+                            <p key={eachLanguage.index}>
+                              nativeName: {eachLanguage.nativeName}
+                            </p>
+                          </>
+                        );
+                      })}
+                    </p>
+                    <hr style={{ width: "100%" }} />
                   </div>
                 );
               })
